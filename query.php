280ba383ca07c311478d8bbdffdd7d6ab4ff51e2
Tim Lightfoot <timlightfoot@Tim-Lightfoots-Mac-mini.local>
2014-10-23 19:06:58 -0500

    Added style to query checker. Practiced using Zurb Foundations.

--- query.php
+++ query.php
@@ -1,99 +1,105 @@
-<!DOCTYPE html>
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <title>Flight finder</title>
+        <meta name="author" content="Tim, Sunny, Pedro, Gilnei" />
+        <meta name="description" content="Flights!" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="css/foundation.css" />
+        <script src="js/vendor/modernizr.js"></script>
+    </head>
+    <body>
+    	<!-- fix the rows and columns for the query checker -->
+    	<div class="row">
+    		<div class="medium-5 small-centered columns">
+    			<h2>query checker</h2>
+    		</div>
+    	</div>
+    	<div class="row">
+    		<fieldset>
+	      		<div class="small-1 columns">
+	      			<table>
+						<thead>
+							<tr>
+								<th>Field</th>
+								<th>Type</th>
+								<th>Example</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>departure</td>
+								<td>varchar(255)</td>
+								<td>ACC</td>
+							</tr>
+							<tr>
+								<td>arrival</td>
+								<td>varchar(255)</td>
+								<td>AMS</td>
+							</tr>
+							<tr>
+								<td>day_op</td>
+								<td>int(11)</td>
+								<td>1234567</td>
+							</tr>
+							<tr>
+								<td>dep_time</td>
+								<td>varchar(255)</td>
+								<td>21:50</td>
+							</tr>
+							<tr>
+								<td>carrier</td>
+								<td>varchar(255)</td>
+								<td>KL</td>
+							</tr>
+							<tr>
+								<td>airline</td>
+								<td>varchar(255)</td>
+								<td>KL</td>
+							</tr>
+							<tr>
+								<td>flightnum</td>
+								<td>int(11)</td>
+								<td>590</td>
+							</tr>
+							<tr>
+								<td>duration</td>
+								<td>int(11)</td>
+								<td>420</td>
+							</tr>
+							<tr>
+								<td>aircraft</td>
+								<td>varchar(255)</td>
+								<td>330</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="medium-6 columns">
+					<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+						<?php 
+							if(isset($_POST["query"]))
+							{
+								$query = $_POST["query"];
+							}
+							else
+							{
+								$query = "";
+							}
+						?>
+						<textarea name="query" rows="19" required><?php echo $query; ?></textarea>
 
-<html>
-	<head>
-    	<title>Stats</title>
-		<meta name="author" content="Tim Lightfoot" />
-		<meta name="description" content="My place on the internets" />
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="../mainStyle.css" type="text/css" />
-		<link rel="icon" type="image/png" href="../favi.png">
-		<script type="text/javascript">
-  			var _gaq = _gaq || [];
-  			_gaq.push(['_setAccount', 'UA-37326147-1']);
-  			_gaq.push(['_trackPageview']);
-	
-  			(function() 
-  			{
-    			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  			})();
-		</script>
-	</head>
-	<body>
-		<div id="page">
-			<div id="content">
-				<h1>flights</h1>
-
-				<table border="1">
-					<tbody>
-						<tr>
-							<th>Field</th>
-							<th>Type</th>
-							<th>Example</th>
-						</tr>
-						<tr>
-							<td>departure</td>
-							<td>varchar(255)</td>
-							<td>ACC</td>
-						</tr>
-						<tr>
-							<td>arrival</td>
-							<td>varchar(255)</td>
-							<td>AMS</td>
-						</tr>
-						<tr>
-							<td>day_op</td>
-							<td>int(11)</td>
-							<td>1234567</td>
-						</tr>
-						<tr>
-							<td>dep_time</td>
-							<td>varchar(255)</td>
-							<td>21:50</td>
-						</tr>
-						<tr>
-							<td>carrier</td>
-							<td>varchar(255)</td>
-							<td>KL</td>
-						</tr>
-						<tr>
-							<td>airline</td>
-							<td>varchar(255)</td>
-							<td>KL</td>
-						</tr>
-						<tr>
-							<td>flightnum</td>
-							<td>int(11)</td>
-							<td>590</td>
-						</tr>
-						<tr>
-							<td>duration</td>
-							<td>int(11)</td>
-							<td>420</td>
-						</tr>
-						<tr>
-							<td>aircraft</td>
-							<td>varchar(255)</td>
-							<td>330</td>
-						</tr>
-					</tbody>
-				</table>
-
-				</br>
-			
-				<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-					Query: <textarea name="query" rows="5" cols="80" required></textarea><br>
-					<input type ="submit">
-				</form>
-				
+						<input type ="submit" class="button small radius">						
+					</form>
+				</div>
+			</fieldset>
+		</div>
+		<div class="row">
+			<div class="small-7 columns">
 				<?php
 					if(isset($_POST["query"]))
 					{
-						$query = $_POST["query"];
-						echo $query . "<br>";
-
 						ini_set('display_errors',0);
 						error_reporting(E_ALL);
 
@@ -106,14 +112,14 @@
 						else if($result = mysqli_query($table, $query))
 						{
 							$fieldInfo = mysqli_fetch_fields($result);
-							echo "<table border='1'> <tr>";
+							echo "<div class='row'> <div class='small-12 small-centered columns'><table> <thead> <tr>";
 
 							foreach($fieldInfo as $column)
 							{
 								echo "<th>" . $column->name . "</th>";
 							}
 
-							echo "</tr>";
+							echo "</tr> </thead>";
 
 							while($row = mysqli_fetch_array($result))
 							{
@@ -126,7 +132,7 @@
 
 								echo "</tr>";
 							}
-							echo "</table>";
+							echo "</table> </div> </div>";
 						}
 						else
 						{
@@ -138,8 +144,13 @@
 					//mysqli_free_result($result);
 					//mysqli_close($table);
 				?>
-				
-			</div>			
-		</div>
-	</body>
+			</div>
+      	</div>
+        
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
 </html>
